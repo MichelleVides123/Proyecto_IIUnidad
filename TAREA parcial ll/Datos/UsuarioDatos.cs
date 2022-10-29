0fe1db81ff6e781a091b2cd5ef2f31ebf01ca897
MichelleVides123 <demaryoryorellana2112@gmail.com>
2022-10-29 14:58:21 -0600

    segunda parte
    parte de la tarea

--- TAREA parcial ll/Datos/UsuarioDatos.cs
+++ TAREA parcial ll/Datos/UsuarioDatos.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Datos
 {
-    internal class UsuarioDatos
+    public class UsuarioDatos
     {
+        public async Task<bool> LoginAsync(string codigo, string clave, string correo)
+        {
+            bool valido = false;
+            try
+            {
+                string sql = "SELECT 1 FROM usuario WHERE Codigo=@codigo AND Clave=@clave AND Correo=@correo";
+
+                using (MySqlConnection _conexion = new MySqlConnection(CadenaConexion.Cadena))
+                {
+                    await _conexion.OpenAsync();
+                    using (MySqlCommand comando = new MySqlCommand(sql, _conexion))
+                    {
+                        comando.CommandType = System.Data.CommandType.Text;
+                        comando.Parameters.Add("@Codigo", MySqlDbType.VarChar, 20).Value = codigo;
+                        comando.Parameters.Add("@Clave", MySqlDbType.VarChar, 20).Value = clave;
+                        comando.Parameters.Add("@Correo", MySqlDbType.VarChar, 20).Value = correo;
+
+                        valido = Convert.ToBoolean(await comando.ExecuteScalarAsync());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return valido;
+        }
+
+
+        public async Task<DataTable> DevolverListaAsync()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = "SELECT * FROM Usuario";
+
+                using (MySqlConnection _conexion = new MySqlConnection(CadenaConexion.Cadena))
+                {
+                    await _conexion.OpenAsync();
+                    using (MySqlCommand comando = new MySqlCommand(sql, _conexion))
+                    {
+                        comando.CommandType = System.Data.CommandType.Text;
+                        MySqlDataReader dr = (MySqlDataReader)await comando.ExecuteReaderAsync();
+                        dt.Load(dr);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
     }
 }
